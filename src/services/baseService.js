93d661b449a8b7bbdeba35193a75060211af231c
zaki-20 <zakibutt199@gmail.com>
2024-07-19 02:51:04 +0500

    add base url tp env file

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { getToken } from '../utils/storageUtils';
 
 // export const API_BASE_URL = 'http://localhost:8000/api/v1/';
-export const API_BASE_URL = `https://luxcars-backendservices.up.railway.app/api/v1/`;
+export const API_BASE_URL = process.env.API_BASE_URL;
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
