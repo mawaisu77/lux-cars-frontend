9e394dbf81ddee0ec958b9f875641c9b51a28c2a
zaki-20 <zakibutt199@gmail.com>
2024-07-19 10:19:31 +0500

    update axios withCredentails

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -7,6 +7,7 @@ export const API_BASE_URL = process.env.API_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true, // Include credentials in requests
   headers: {
     'Content-Type': 'application/json',
   },
