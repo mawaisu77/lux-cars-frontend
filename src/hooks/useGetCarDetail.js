7ca72a362dedc22117a99d60be0828e09cd8143c
zaki-20 <zakibutt199@gmail.com>
2024-07-31 16:52:46 +0500

    custom hook for get car detail

---
+++ src/hooks/useGetCarDetail.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import baseService from '../services/baseService';
+
+const useGetAllBidsCar = (url) => {
+  const [carDetailData, setCarDetailData] = useState(null);
+  const [carDetailLoading, setCarDetailLoading] = useState(true);
+  const [carDetailError, setCarDetailError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await baseService.get(url);
+        setCarDetailData(response.data);
+      } catch (err) {
+        if (err.response) {
+          setCarDetailError(`${err.response.data.message}`);
+        } else if (err.request) {
+          setCarDetailError('Error: No response from server');
+        } else {
+          setCarDetailError(`Error: ${err.message}`);
+        }
+      } finally {
+        setCarDetailLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { carDetailData, carDetailLoading, carDetailError };
+};
+
+export default useGetAllBidsCar;
