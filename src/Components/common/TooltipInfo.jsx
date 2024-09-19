6b8c17c76aac8148a6098e0776a6ab8cec1d69d2
zaki-20 <zakibutt199@gmail.com>
2024-09-19 17:15:59 +0500

    tooltip fix

--- src/Components/common/TooltipInfo.jsx
+++ src/Components/common/TooltipInfo.jsx
@@ -6,7 +6,7 @@ const TooltipInfo = ({ content, children }) => {
     <div className="relative inline-block">
     <div className="cursor-pointer group ">
       {children || <BsInfoCircle className='hover:text-blue-800 duration-200'/>}
-      <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-64 p-4 bg-gray-800 text-white text-sm rounded-lg shadow-lg opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-200">
+      <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-56 p-3 bg-gray-800 text-white text-xs rounded-lg shadow-lg opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-200">
         {content}
         <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-2 h-2 bg-gray-800 rotate-45"></div>
       </div>
