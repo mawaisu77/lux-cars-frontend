bb0d61d41751bd8c953aa1385f0f5f280e76c2f1
zaki-20 <zakibutt199@gmail.com>
2024-10-31 22:35:36 +0500

    all section cars home page fixed

--- src/Components/cards/BidCarsSection.js
+++ src/Components/cards/BidCarsSection.js
@@ -16,7 +16,7 @@ const BidCarsSection = () => {
   const navigate = useNavigate(); 
 
   const { carData, carLoading, carError } = useGetAllBidsCar(
-    "bid-cars/find-bid-cars"
+    "bid-cars/find-bid-cars?size=8"
   );
   if (carLoading) {
     return <Shimmer />;
@@ -38,17 +38,17 @@ const BidCarsSection = () => {
 
   return (
     <>
-      <div className="relative w-full lg:w-[98.9vw] bg-light-gray   ">
+      <div className="relative w-full lg:w-[98.9vw] bg-light-gray py-10">
         <div className="" id="startBidding">
-          <div className="sm:pl-2 px-4 w-full sm:w-[85vw] flex justify-between items-center md:w-[88vw] lg:w-[82vw] mx-auto ">
+        <div className=" w-full sm:w-[85vw] flex justify-between items-center mx-auto ">
             <div className="flex flex-col gap-y-2">
-              <div className="flex justify-start font-urbanist text-[26px] text-lux-black lg:text-36 font-bold leading-[2vw] pt-[2vh]">
-                Bid Cars
+              <div className="flex justify-start items-center font-urbanist text-[26px] lg:text-34 font-bold leading-[2vw] pt-[2vh]">
+                Bid Car
               </div>
-              <hr className="h-1 bg-primary-red mt-[4px] w-16 " />
+              <hr className="h-1 bg-primary-red w-16 " />
             </div>
 
-            <div className="flex justify-center text-primary-red  items-center gap-x-1">
+            <div className="flex justify-center text-primary-red text-18  items-center gap-x-1">
               <button
                 onClick={handleViewAllClick}
                 className="font-bold text-sm hover:underline"
@@ -58,69 +58,13 @@ const BidCarsSection = () => {
               <FaExternalLinkAlt size={13} />
             </div>
           </div>
-
-          <div className="relative sm:px-0 px-10 mt-5">
-            <Swiper
-              style={{
-                "--swiper-pagination-color": "#FFBA08",
-                "--swiper-pagination-bullet-inactive-color": "#999999",
-                "--swiper-pagination-bullet-inactive-opacity": "1",
-                "--swiper-pagination-bullet-size": "12px",
-                "--swiper-pagination-bullet-horizontal-gap": "3px",
-              }}
-              // slidesPerView={1}
-              spaceBetween={0}
-              breakpoints={{
-                440: { slidesPerView: 1 },
-                580: { slidesPerView: 2 },
-                640: { slidesPerView: 3 },
-                840: { slidesPerView: 4 },
-                1024: { slidesPerView: 4 },
-                1200: { slidesPerView: 5 },
-                1440: { slidesPerView: 5 },
-                1620: { slidesPerView: 5 },
-                1920: { slidesPerView: 5 },
-              }}
-              scrollbar={{ draggable: true }}
-              loop={true}
-              navigation={{
-                nextEl: ".arrow-right-bid",
-                prevEl: ".arrow-left-bid",
-              }}
-              pagination={{
-                clickable: true,
-                renderBullet: (index, className) => {
-                  if (index < 4) {
-                    return `<span class="${className}"></span>`;
-                  }
-                  return "";
-                },
-              }}
-              modules={[Navigation, Pagination]}
-              className="w-full sm:w-[85vw] md:w-[88vw] lg:w-[82vw] "
-            >
-              <div className="">
+          <div className="relative mt-4 mx-auto sm:w-[85vw] gap-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
                 {carData &&
                   carData.map((card, index) => (
-                    <SwiperSlide key={index} className="relative py-5 mb-5">
-                      <CarCard card={card} isBuy={false} />
-                    </SwiperSlide>
+                      <CarCard key={index} card={card} isBuy={false} />
                   ))}
-              </div>
-            </Swiper>
           </div>
-          <button className="arrow-left-bid arrow-bid sm:block">
-            <IoIosArrowDropleft
-              size={28}
-              className="text-[#ca0000] hover:bg-[#ca0000] rounded-full hover:text-white duration-150"
-            />
-          </button>
-          <button className="arrow-right-bid arrow-bid sm:block ">
-            <IoIosArrowDropright
-              size={28}
-              className="text-[#ca0000] hover:bg-[#ca0000] rounded-full hover:text-white duration-150"
-            />
-          </button>
+       
         </div>
       </div>
     </>
