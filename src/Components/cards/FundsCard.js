209c350c9559fd0f7fe13d5e4e03a1bac8bb3acf
hashir-aziz <hashiraziz488@gmail.com>
2025-03-04 11:25:08 -0800

    user screens

--- src/Components/cards/FundsCard.js
+++ src/Components/cards/FundsCard.js
@@ -10,7 +10,7 @@ const FundsCard = ({fund}) => {
   const { totalDeposits = 0, avalaibleBidAmount = 0, avalaibleBids = 0, usedBidAmount = 0, activeBids = 0 } = fund || {};
   const FundsC = [
     {
-      icon: <BiSolidWallet color='#7e72f2' size={20} />,
+      icon: <BiSolidWallet color='#7e72f2' size={20}  className='lg:w-[1vw] lg:h-[1vw]' />,
       States: "Your Total Deposits",
       Amount: "$"+ totalDeposits || null,
       Lorem: "Lorem Ipsum",
@@ -18,7 +18,7 @@ const FundsCard = ({fund}) => {
       bgColor: '#d5d1fb'
     },
     {
-      icon: <BiSolidWallet color='#2dcd7a' size={20} />,
+      icon: <BiSolidWallet color='#2dcd7a'  size={20}  className='lg:w-[1vw] lg:h-[1vw]' />,
       States: "Bids Available",
       Amount: "" + avalaibleBids || null,
       Lorem: "Lorem Ipsum",
@@ -26,7 +26,7 @@ const FundsCard = ({fund}) => {
       bgColor: '#e6f7ee'
     },
     {
-      icon: <BiSolidWallet color='#ff9f43' size={20} />,
+      icon: <BiSolidWallet color='#ff9f43'  size={20}  className='lg:w-[1vw] lg:h-[1vw]' />,
       States: "Active Bid",
       Amount: "" + activeBids || null,
       Lorem: "Lorem Ipsum",
@@ -34,7 +34,7 @@ const FundsCard = ({fund}) => {
       bgColor: '#fceaea'
     },
     {
-      icon: <BiSolidWallet color='#ed5f5f' size={20} />,
+      icon: <BiSolidWallet color='#ed5f5f'  size={20}  className='lg:w-[1vw] lg:h-[1vw]' />,
       States: "Bid Amount Available",
       Amount: "$"+ avalaibleBidAmount || null,
       Lorem: "Lorem Ipsum",
@@ -43,7 +43,7 @@ const FundsCard = ({fund}) => {
       
     },
     {
-      icon: <BiSolidWallet color='#ff9f43' size={20} />,
+      icon: <BiSolidWallet color='#ff9f43'  size={20}  className='lg:w-[1vw] lg:h-[1vw]'/>,
       States: "Bid Amount Used",
       Amount: "$"+ usedBidAmount || null,
       Lorem: "Lorem Ipsum",
@@ -53,12 +53,12 @@ const FundsCard = ({fund}) => {
   ];
 
   return (
-    <div className='w-full   max-w-[1370px]  justify-center lg:max-w-[84vw] mx-auto  flex flex-wrap gap-4 mt-[30px] px-4'>
+    <div className='w-full   max-w-[1370px]  justify-center lg:max-w-[84vw] mx-auto  flex flex-wrap gap-4 lg:gap-[1vw] mt-[30px] px-4 lg:px-[1vw]'>
       {FundsC.map((item, index) => (
-        <div key={index} className=' relative w-full md:w-[300px] h-[270px] lg:h-[27vh] border shadow-md rounded-xl flex-shrink-0'>
-          <div className='flex items-center gap-2 p-4'>
+        <div key={index} className=' relative w-full lg:w-[15.25vw] h-[270px] lg:h-[27vh] border shadow-md rounded-xl lg:rounded-[0.75vw] flex-shrink-0'>
+          <div className='flex items-center gap-2 lg:gap-[0.5vw] p-4'>
             <div
-              className='flex justify-center items-center p-2 rounded-full'
+              className='flex justify-center items-center p-2 lg:p-[0.5vw] rounded-full lg:rounded-[100%]'
               style={{ backgroundColor: item.bgColor }}
             >
               {item.icon}
